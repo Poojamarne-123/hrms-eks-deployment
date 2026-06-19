26f572fb13e47eaf664b7363228c8820d254a9c1
root <root@ip-172-31-47-248.ec2.internal>
2026-06-19 22:47:51 +0000

    Final EKS deployment fixes

--- frontend/src/components/environment.ts
+++ frontend/src/components/environment.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: '/api',
+  apiBaseUrl: 'http://a2b87bab7896a40d19b8854c709dd6a5-1678564510.us-east-1.elb.amazonaws.com:8080/api',
   appName: 'HRMS',
   enableDebug: false
 };
- 
- 
